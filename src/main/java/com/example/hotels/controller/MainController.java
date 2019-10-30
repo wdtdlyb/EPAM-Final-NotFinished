b46b89bc6c85c44846cb4edeccbbe526632bbdc2
Aliaksei Hudyma <allanhaff@gmail.com>
2019-10-30 22:36:50 +0300

    Login & auth added

--- src/main/java/com/example/hotels/controller/MainController.java
+++ src/main/java/com/example/hotels/controller/MainController.java
@@ -1,4 +1,4 @@
-package com.example.hotels;
+package com.example.hotels.controller;
 
 import com.example.hotels.domain.Message;
 import com.example.hotels.repos.MessageRepo;
@@ -11,20 +11,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.Map;
 
 @Controller
-public class GreetingController {
+public class MainController {
     @Autowired
     private MessageRepo messageRepo;
 
-    @GetMapping("/greeting")
-    public String greeting(
-            @RequestParam(name="name", required=false, defaultValue="World") String name,
-            Map<String, Object> model
-    ) {
-        model.put("name", name);
+    @GetMapping("/")
+    public String greeting(Map<String, Object> model) {
         return "greeting";
     }
 
-    @GetMapping
+    @GetMapping("/main")
     public String main(Map<String, Object> model) {
         Iterable<Message> messages = messageRepo.findAll();
 
@@ -33,7 +29,7 @@ public class GreetingController {
         return "main";
     }
 
-    @PostMapping
+    @PostMapping("/main")
     public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
         Message message = new Message(text, tag);
 
